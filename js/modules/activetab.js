48385a1196cfd2a56b2148c82cd03430efe177e3
Higor Almeida <idanger2015@hotmail.com>
2020-10-30 16:03:52 -0300

    TabNav refatorado

--- js/modules/activetab.js
+++ js/modules/activetab.js
@@ -1,26 +1,33 @@
-export default function activeTab(){
-    const faq = document.querySelector('.faq');
-    const animais = document.querySelector('.animais');
+export default class TabNav {
+  constructor(menu, content) {
+    this.menu = document.querySelectorAll(menu);
+    this.content = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
+  }
 
-    faq.innerHTML;
-
-    const menuTab = document.querySelectorAll('[data-tab="menu"] li');
-    const menuSection = document.querySelectorAll('[data-tab="content"] section');
-    document.documentElement.className = 'js';
-    if (menuTab.length  && menuSection.length > 0){
-        menuSection[0].classList.add('ativo');
-    }
-    function activeTab(index){
-        menuSection.forEach((section) => {
-            section.classList.remove('ativo');
-        });
-        const direcao = menuSection[index].dataset.anima;
-        menuSection[index].classList.add('ativo', direcao);
-    }
-    
-    menuTab.forEach((item, index) => {
-        item.addEventListener('click', () => {
-            activeTab(index);
-        });
+  // ative o tabnav de acordo com o index
+  activeTab(index) {
+    this.content.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
+    const direcao = this.content[index].dataset.anima;
+    this.content[index].classList.add(this.activeClass, direcao);
+  }
+
+  // Adiciona evento ao tabnav
+
+  addTabEvent() {
+    this.menu.forEach((item, index) => {
+      item.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  // Inicia o tabnav
+  init() {
+    document.documentElement.className = 'js';
+    if (this.menu.length && this.content.length > 0) {
+      this.activeTab(0);
+      this.addTabEvent();
+    }
+  }
 }
