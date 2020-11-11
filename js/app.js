d30f0c408e9b5c6eeea649119bc6003b56cf87fc
Higor Almeida <idanger2015@hotmail.com>
2020-11-11 20:24:03 -0300

    Refazendo e refatorando AnimaScroll

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
-import ScrollSuave from './modules/scroll-suave.js';
-import Accordion from './modules/accordion.js';
-import TabNav from './modules/activetab.js';
-import Modal from './modules/modal.js';
-import initIrTopPage from './modules/backtop.js';
-import Tooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
-import FetchBitcoin from './modules/fetchBitcoin.js';
-import initAnimaScroll from './modules/anima-scroll.js';
-import fetchAnimais from './modules/fetchAnimais.js';
+import ScrollSuave from "./modules/scroll-suave.js";
+import Accordion from "./modules/accordion.js";
+import TabNav from "./modules/activetab.js";
+import Modal from "./modules/modal.js";
+import initIrTopPage from "./modules/backtop.js";
+import Tooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import FetchBitcoin from "./modules/fetchBitcoin.js";
+import initAnimaScroll from "./modules/anima-scroll.js";
+import fetchAnimais from "./modules/fetchAnimais.js";
+import AnimaScroll from "./modules/anima-scroll.js";
 
 const scrollSuave = new ScrollSuave('[data-anima="menu-suave"] a[href^="#"]');
 scrollSuave.init();
@@ -17,13 +18,20 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anima="accordion"] dt');
 accordion.init();
 
-const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
 tabNav.init();
 
-const modal = new Modal('[data-modal="container"]', '[data-modal="abrir"]', '[data-modal="fechar"]');
-modal.init(); 
+const modal = new Modal(
+  '[data-modal="container"]',
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]'
+);
+modal.init();
 
-const tooltip = new Tooltip('[data-tooltip]');
+const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
 
 initIrTopPage();
@@ -31,10 +39,13 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 
-const fetchBitcoin = new FetchBitcoin('https://blockchain.info/ticker', '.bitcoin');
+const fetchBitcoin = new FetchBitcoin(
+  "https://blockchain.info/ticker",
+  ".bitcoin"
+);
 fetchBitcoin.init();
 
-initAnimaScroll();
+const animaScroll = new AnimaScroll('[data-anima="scroll"]');
+animaScroll.init();
 
-fetchAnimais('animaisapi.json', '.numeros-grid');
-
+fetchAnimais("animaisapi.json", ".numeros-grid");
--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -1,16 +1,41 @@
-export default function initAnimaScroll() {
-    const sections = document.querySelectorAll('[data-anima="scroll"]');
-    function animaScroll(){
-        sections.forEach((section) =>{
-            const sectionTop = section.getBoundingClientRect().top;
-            const sectionVisible = (sectionTop - (window.innerHeight * 0.6)) < 0;
-            if (sectionVisible) {
-                section.classList.add('ativo');
-            }
-        });
-    }
-    animaScroll();
-    
-    window.addEventListener('scroll', animaScroll); 
-}
+export default class AnimaScroll {
+  constructor(target) {
+    this.target = document.querySelectorAll(target);
+    this.windowMetade = window.innerHeight * 0.6;
+    this.checkDistance = this.checkDistance.bind(this);
+  }
 
+  // Pega a distância dos itens/sections
+
+  getDistance() {
+    this.distance = [...this.target].map(section => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: offset - this.windowMetade,
+      };
+    });
+    console.log(this.distance);
+  }
+
+  // Verifica se a distância do item é maior que a do PageYOfsset e adiciona classe ativo
+
+  checkDistance() {
+    this.distance.forEach(item => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      }
+    });
+  }
+
+  // Inicia a função inteira
+
+  init() {
+    if (this.target.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+}
